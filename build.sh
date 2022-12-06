ba08cb89d71b5fbea08516f61a946563a1453abd
Emile Sonneveld <contact@emilesonneveld.be>
2022-12-06 02:03:34 +0100

    Quote around semicolon.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir cmake_build
 cd cmake_build
-cmake .. -DCMAKE_CONFIGURATION_TYPES=Debug;RelWithDebInfo
+cmake .. -DCMAKE_CONFIGURATION_TYPES="Debug;RelWithDebInfo"
 cmake --build . --config Debug
